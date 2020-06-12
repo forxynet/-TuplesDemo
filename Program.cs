98dd9ee5a81674a0c289430c487543314ba9a0d9
forxynet <forxynet@gmail.com>
2020-06-12 22:27:42 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,29 +1,34 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data;
 
-namespace TupleDemo {
+namespace TuplesDemo {
     class Program {
         static void Main(string[] args) {
             var values = new List<double> { 10, 20, 30, 40, 50 };
             Tuple<int, double> tuple = Calculate(values);
             Console.WriteLine($"their count is {tuple.Item1} There sum is {tuple.Item2}");
-            //var result = Calulate(values);
-            //Console.WriteLine($"their sum is {result.sum} There are {result.count} values");
-            var (count, sum) = Calulate(values);
-            Console.WriteLine($"There are {count} values and their sum is {sum}");
+
+            var result = Calulate(values);
+            Console.WriteLine($"their sum is {result.Item1} There are {result.Item3} values");
+
+            var (count, sum, resultMessage) = Calulate(values);
+            if (resultMessage!="") {
+                Console.WriteLine($"There are {count} values and their sum is {sum}");
+            }
+            
 
             Console.ReadKey();
         }
 
-        private static (int , double) Calulate(IEnumerable<double> values) {
+        private static (int count, double sum, string message) Calulate(IEnumerable<double> values) {
             int count = 0;
             double sum = 0.0;
             foreach (var value in values) {
                 count++;
                 sum += value;
             }  
-            return (count, sum);
+            return (count, sum, "success");
         }
 
         private static Tuple<int, double> Calculate(List<double> values) {
